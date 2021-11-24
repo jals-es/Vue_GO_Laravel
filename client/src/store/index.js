d4196b0a748f0b5664dd982b53f4b0f40a1e8e01
RTPablocs <conejerosorianopablo@gmail.com>
2021-11-24 21:25:51 +0100

    Added Store for superUser

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,12 +1,10 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
+import user from './superUser'
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+
+    modules: {
+        user
+    }
+
 })
