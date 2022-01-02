1a69599f0c8ac5030de9be77f57c18b5118936df
RTPablocs <conejerosorianopablo@gmail.com>
2022-01-02 17:28:18 +0100

    Arranged Routes

--- server/GO/role_permissions/router.go
+++ server/GO/role_permissions/router.go
@@ -7,13 +7,17 @@ import (
 
 func PrRoutes(router *gin.RouterGroup) {
 	router.Use(users.AuthMiddleware(false))
+
 	router.POST("/new-role", CreateRole)
 	router.GET("/roles", ListRoles)
-	router.DELETE("/role/*id")
+	router.DELETE("/role/*id", DisableRole)
+	router.GET("/role/*id")
+
 	router.POST("/new-permission", CreatePermission)
 	router.GET("/permissions", ListPermissions)
-	router.POST("/assignation")
 	router.DELETE("/permission/*id", DeletePermission)
 	router.GET("/permission/*id", GetDataFromPermission)
 
+	router.POST("/assignation")
+	router.DELETE("/assignation")
 }
