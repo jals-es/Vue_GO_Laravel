d684ab64bbc910f3f86b9a6658b948eaef6c9e34
RTPablocs <conejerosorianopablo@gmail.com>
2022-01-10 19:56:41 +0100

    Removed Validator Rule

--- server/GO/bars/validators.go
+++ server/GO/bars/validators.go
@@ -8,11 +8,11 @@ import (
 
 type BarModelValidator struct {
 	Bar struct {
-		Name   	string `form:"name" json:"name" binding:"required,min=8,max=255"`
-		Descr  	string `form:"descr" json:"descr" binding:"required"`
-		Lat    	string `form:"lat" json:"lat" binding:"required"`
-		Lon	   	string `form:"lon" json:"lon" binding:"required"`
-		City   	string `form:"city" json:"city" binding:"required"`
+		Name    string `form:"name" json:"name" binding:"required"`
+		Descr   string `form:"descr" json:"descr" binding:"required"`
+		Lat     string `form:"lat" json:"lat" binding:"required"`
+		Lon     string `form:"lon" json:"lon" binding:"required"`
+		City    string `form:"city" json:"city" binding:"required"`
 		Address string `form:"address" json:"address" binding:"required"`
 	} `json:"bar"`
 	barModel BarModel `json:"-"`
@@ -24,16 +24,16 @@ func (bar *BarModelValidator) Bind(c *gin.Context) error {
 	if err != nil {
 		return err
 	}
-	
+
 	bar.barModel.Name = bar.Bar.Name
 	bar.barModel.Descr = bar.Bar.Descr
 	bar.barModel.Lat = bar.Bar.Lat
 	bar.barModel.Lon = bar.Bar.Lon
 	bar.barModel.City = bar.Bar.City
 	bar.barModel.Address = bar.Bar.Address
-	
+
 	myUserModel := c.MustGet("my_user_model").(users.UserModel)
-	
+
 	bar.barModel.Owner = myUserModel.ID
 
 	return nil
