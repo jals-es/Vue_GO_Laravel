688ddd986def5beec7087cf6e1d2f0e750157d57
Juan Antonio Lopez Segui <juan.antonio.lis@gmail.com>
2022-01-12 18:38:38 +0100

    create product

--- client/src/composables/prods.js
+++ client/src/composables/prods.js
@@ -1,4 +1,5 @@
 import golangApiService from "@/core/http/golang.api.service";
+import router from '@/router'
 import { reactive } from 'vue';
 export function prods() {
 
@@ -11,18 +12,27 @@ export function prods() {
         }
     }
 
-    const deleteProd = async(slug, bar_slug) => {
+    const deleteProd = async(slug, bar_slug, index) => {
         let delProds = await golangApiService.delete("/api/prod/" + slug + "/")
         if (delProds) {
             console.log(delProds.data.message)
             console.log(bar_slug)
-                // getProds(bar_slug)
+            products.prods.splice(index, 1)
+        }
+    }
+
+    const createProd = async(prod, bar_slug) => {
+        let create = await golangApiService.post("/api/prod/", { barslug: bar_slug, prod: prod })
+        if (create.data.message) {
+            products.prods.push(prod)
+            router.push(`/${bar_slug}/admin/`)
         }
     }
 
     return {
         getProds,
         deleteProd,
+        createProd,
         products
     }
 }
--- client/src/views/DashboardBar.vue
+++ client/src/views/DashboardBar.vue
@@ -20,7 +20,7 @@
             </tr>
           </thead>
           <tbody>
-            <tr class="prod" v-for="prod in products.prods" :key="prod.slug" @click="goProd(prod.slug)">
+            <tr class="prod" v-for="(prod, index) in products.prods" :key="prod.slug">
               <td>{{ prod.slug }}</td>
               <td>{{ prod.name }}</td>
               <td>{{ prod.descr }}</td>
@@ -28,7 +28,7 @@
               <td>{{ prod.extras.length }}</td>
               <td><button class="btn btn-success" data-bs-toggle="modal" :data-bs-target="`#${prod.slug}`">Ver</button></td>
               <td>
-                <button class="btn btn-outline-danger" @click="delProd(prod.slug, bar_slug)">Borrar</button>
+                <button class="btn btn-outline-danger" @click="delProd(prod.slug, bar_slug, index)">Borrar</button>
               </td>
               <DashboardViewProd :prod="prod"></DashboardViewProd>
             </tr>
@@ -63,18 +63,13 @@ export default {
 
         getProds(bar_slug)
 
-        const goProd = (/*prod_slug*/) => {
-            // router.push(`/${bar_slug}/admin/${prod_slug}/`)
-        }
-
-        const delProd = (slug, bar_slug) => {
-            deleteProd(slug, bar_slug)
+        const delProd = (slug, bar_slug, index) => {
+            deleteProd(slug, bar_slug, index)
         }
 
         return {
             bar_slug,
             products,
-            goProd,
             delProd
         }
     }
@@ -83,97 +78,7 @@ export default {
 
 <style>
 
-/*
- * Sidebar
- */
 
-.sidebar {
-  position: fixed;
-  top: 0;
-  /* rtl:raw:
-  right: 0;
-  */
-  bottom: 0;
-  /* rtl:remove */
-  left: 0;
-  z-index: 100; /* Behind the navbar */
-  padding: 48px 0 0; /* Height of navbar */
-  box-shadow: inset -1px 0 0 rgba(0, 0, 0, .1);
-}
-
-@media (max-width: 767.98px) {
-  .sidebar {
-    top: 5rem;
-  }
-}
-
-.sidebar-sticky {
-  position: relative;
-  top: 0;
-  height: calc(100vh - 48px);
-  padding-top: .5rem;
-  overflow-x: hidden;
-  overflow-y: auto; /* Scrollable contents if viewport is shorter than content. */
-}
-
-.sidebar .nav-link {
-  font-weight: 500;
-  color: #333;
-}
-
-.sidebar .nav-link i {
-  margin-right: 4px;
-  padding: 20px;
-  color: #727272;
-}
-
-.sidebar .nav-link.active {
-  color: #2470dc;
-}
-
-.sidebar .nav-link:hover .feather,
-.sidebar .nav-link.active .feather {
-  color: inherit;
-}
-
-.sidebar-heading {
-  font-size: .75rem;
-  text-transform: uppercase;
-}
-
-/*
- * Navbar
- */
-
-.navbar-brand {
-  padding-top: .75rem;
-  padding-bottom: .75rem;
-  font-size: 1rem;
-  background-color: rgba(0, 0, 0, .25);
-  box-shadow: inset -1px 0 0 rgba(0, 0, 0, .25);
-}
-
-.navbar .navbar-toggler {
-  top: .25rem;
-  right: 1rem;
-}
-
-.navbar .form-control {
-  padding: .75rem 1rem;
-  border-width: 0;
-  border-radius: 0;
-}
-
-.form-control-dark {
-  color: #fff;
-  background-color: rgba(255, 255, 255, .1);
-  border-color: rgba(255, 255, 255, .1);
-}
-
-.form-control-dark:focus {
-  border-color: transparent;
-  box-shadow: 0 0 0 3px rgba(255, 255, 255, .25);
-}
 
 .prod td{
     width: 20%;
